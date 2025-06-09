68913c26a0d9e8687c7013fa1be34413f6bc6b29
Diogo-SoaresDS <diogo.soares10.ds.ds@gmail.com>
2025-06-09 12:54:17 -0300

    Componente Button Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { PlayCircleIcon } from 'lucide-react'
 import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
 import { Input } from './components/Input'
+import { Cycles } from './components/Cycles'
+import { Button } from './components/Button'
 
 import './styles/theme.css'
 import './styles/global.css'
-import { Cycles } from './components/Cycles'
 
 export function App() {
   return (
@@ -43,7 +45,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <button>Enviar</button>
+            <Button icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
