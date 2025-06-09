42a961e356ff7177698eedf3632659331cd22c5e
Diogo-SoaresDS <diogo.soares10.ds.ds@gmail.com>
2025-06-09 10:32:30 -0300

    Componente Container Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
+import { Container } from './components/Container'
 import { Heading } from './components/Heading'
 
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react'
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium
-        tenetur, saepe beatae adipisci aliquam exercitationem quo animi aut eum
-        non! Ea, sint nam exercitationem reiciendis laborum voluptatem eveniet
-        nesciunt quibusdam.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   )
 }
